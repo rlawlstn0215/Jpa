b9d9cd63dff51a44500d555a98dc427f1ea5c87d
rlawlstn0215 <58507948+rlawlstn0215@users.noreply.github.com>
2020-06-24 17:45:58 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().mvcMatchers("/", "/login", "/join", "/check-email-token", "/email-login",
+                "/check-email", "/login-link", "/check-email-login").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();
+    }
+}
